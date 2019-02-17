15d03ce59dd306a4ef7df05d83dacaa628ba66df
BulatTH <bulatxajrutdinoff@yandex.ru>
2019-02-17 11:55:11 +0300

    find #2

--- app/Db.php
+++ app/Db.php
@@ -29,26 +29,50 @@ class Db
 //        for ($a = 1; $a < 11; $a+=1 ) {
 //            $bookArr[$a] = R::dispense('book');
 //            $bookArr[$a]->title = "Book #{$a}";
-//            $bookArr[$a]->price = $a + rand(1,5);
+//            $bookArr[$a]->price = $a;
+//            $bookArr[$a]->category_id = rand(1,5);
 //            R::store($bookArr[$a]);
 //        }
+//
+//        R::wipe("category");
+//        $categoryArr = [];
+//        for ($a = 1; $a < 6; $a+=1 ) {
+//            $categoryArr[$a] = R::dispense('category');
+//            $categoryArr[$a]->category = "Category #" . $a;
+//            R::store($categoryArr[$a]);
+//        }
 
-//        $books = R::find('book', 'price > ?', [10]);
-//        $books = R::find('book', 'title LIKE ?', ['%#10%']);
-
-//        $books = R::find('book', 'id > ? AND price > ?', [7, 10]);
-//        $books = R::find('book', 'id > :id AND price > :price', [":id" => 7, ":price" => 10]);
-
-//        $ids = [1,3,5];
-//        $books = R::find('book', 'id IN (?, ?, ?)', $ids);
-//        $books = R::find('book', 'id IN ('.R::genSlots($ids).')', $ids);
-
-
-//        $book = R::findOne('book', 'id > ?', [8]);
 //        $books = R::findAll('book');
 
-        R::hunt('book', 'id > ?', [5]);
-        
+//        $collection = R::findCollection('book');
+//        while ($item = $collection->next()) {
+////            debug($item);
+//            $item->price = 3;
+//            R::store($item);
+//        }
+
+//        debug($collection);
+
+//        $books = R::findLike('book',
+//            ['title'=>[ "Book #1", "Book #9"]]
+//        );
+//        debug($books);
+
+
+//        $book = R::findOrCreate("book", [
+//            'title'=>'my book',
+//            'price' => 3.99
+//        ]);
+//        debug($book);
+
+        $booksWithCategory = R::findMulti('book,category', '
+            SELECT book.*, category.* FROM book
+            INNER JOIN category ON category.id=book.category_id
+            WHERE category.id=:cat_id
+        ', [':cat_id'=>2]);
+
+        debug($booksWithCategory);
+
     }
 
 }
