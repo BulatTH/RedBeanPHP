564085faeaa0454e117f17884689e4d230e4ff40
BulatTH <bulatxajrutdinoff@yandex.ru>
2019-02-17 10:25:50 +0300

    find #1

--- app/Db.php
+++ app/Db.php
@@ -24,32 +24,30 @@ class Db
             die();
         }
 
-
-//        $book = R::load('book', 3);
-//        $book = $book->export();
-//        debug($book);
-
-        /* Retrieve */
-
-//        $books = R::loadAll('book', [1,3]);
-//        debug($books);
-
-        /* DELETE */
-
-//        $newBook = R::dispense('book');
-//        $newBook->title="Bright2 Book";
-//        $newBook->price = 5;
-//        R::store($newBook);
-
-//        $book = R::load('book', 5); //green book
-//        R::trash($book);
-
-//        $books = R::loadAll('book', [4,6]);
-//        R::trashAll($books);
-
-//        R::trashBatch('book', [7,8]);
-
 //        R::wipe('book');
+//        $bookArr = [];
+//        for ($a = 1; $a < 11; $a+=1 ) {
+//            $bookArr[$a] = R::dispense('book');
+//            $bookArr[$a]->title = "Book #{$a}";
+//            $bookArr[$a]->price = $a + rand(1,5);
+//            R::store($bookArr[$a]);
+//        }
+
+//        $books = R::find('book', 'price > ?', [10]);
+//        $books = R::find('book', 'title LIKE ?', ['%#10%']);
+
+//        $books = R::find('book', 'id > ? AND price > ?', [7, 10]);
+//        $books = R::find('book', 'id > :id AND price > :price', [":id" => 7, ":price" => 10]);
+
+//        $ids = [1,3,5];
+//        $books = R::find('book', 'id IN (?, ?, ?)', $ids);
+//        $books = R::find('book', 'id IN ('.R::genSlots($ids).')', $ids);
+
+
+//        $book = R::findOne('book', 'id > ?', [8]);
+//        $books = R::findAll('book');
+
+        R::hunt('book', 'id > ?', [5]);
         
     }
 
