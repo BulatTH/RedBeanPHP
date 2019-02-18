6349304481b75af94a575f65309246511b2294cc
BulatTH <bulatxajrutdinoff@yandex.ru>
2019-02-18 14:44:18 +0300

    Transactions

--- app/Db.php
+++ app/Db.php
@@ -18,7 +18,9 @@ class Db
     private function __construct()
     {
         $db_conf = require_once CONF . "/db_conf.php";
+        $db_conf2 = require_once CONF . "/db_conf2.php";
         R::setup($db_conf['dsn'], $db_conf['user'], $db_conf['password'], true);
+
         if (!R::testConnection()) {
             throw new \Exception("Ошибка подключения", 500);
             die();
@@ -26,41 +28,40 @@ class Db
 
         R::debug(1);
 
+//        $fields = R::inspect("book");
+//        debug($fields);
+
+//        $listOfTables = R::inspect();
+//        debug($listOfTables);
 
 
-//        $data = R::getLook()->look(
-//            "SELECT id, title FROM book WHERE id > ?",
-//            [5],
-//            ['id', 'title'],
-//            "\t<li value='%s'>%s</li>", 'strtoupper', "\n"
-//        );
-//
-//        echo "<ul>\n{$data}\n</ul>";
+//        R::addDatabase("db2", $db_conf2['dsn'], $db_conf2['user'], $db_conf2['password'], true);
+//        R::selectDatabase("db2");
 
-//        $book = null;
-//        $didChangePrice = R::matchUp(
-//            "book",
-//            " title = ?",
-//            ["Test book Title 1020"],
-//            [
-//                "price" => "0"
-//            ],
-//            [
-//                "title" => "Test book Title 1020",
-//                "price" => 0,
-//                "category_id" => 1
-//            ],
-//            $book
-//        );
-//        debug($book);
+//        $listOfTables2 = R::inspect();
+//        debug($listOfTables2);
 
-//        R::csv(
-//            "SELECT id, title FROM book WHERE id > ?",
-//            [8],
-//            ["ID", "TITLE"],
-//            __DIR__ . "/file.csv"
-//        );
+        /*
+        $book = R::dispense("book");
+        $category = R::dispense("category");
 
+        $category->category = "bulat's category";
+
+        R::begin();
+        try {
+            $category_id = R::store($category);
+
+            $book->title = "bulat's first book";
+            $book->price = 10;
+            $book->category_id = $category_id;
+
+            R::store($book);
+            R::commit();
+        } catch (\Exception $e) {
+            R::rollback();
+            echo $e->getMessage();
+        }
+        */
 
     }
 
