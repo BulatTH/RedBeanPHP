6d98d74f307a5404d9bb3358fa6d41fcca04019f
BulatTH <bulatxajrutdinoff@yandex.ru>
2019-02-18 14:08:58 +0300

    Data Tools

--- app/Db.php
+++ app/Db.php
@@ -24,48 +24,43 @@ class Db
             die();
         }
 
-//        R::wipe('book');
-//        $bookArr = [];
-//        for ($a = 1; $a < 11; $a+=1 ) {
-//            $bookArr[$a] = R::dispense('book');
-//            $bookArr[$a]->title = "Book #{$a}";
-//            $bookArr[$a]->price = $a;
-//            $bookArr[$a]->category_id = rand(1,5);
-//            R::store($bookArr[$a]);
-//        }
-//
-//        R::wipe("category");
-//        $categoryArr = [];
-//        for ($a = 1; $a < 6; $a+=1 ) {
-//            $categoryArr[$a] = R::dispense('category');
-//            $categoryArr[$a]->category = "Category #" . $a;
-//            R::store($categoryArr[$a]);
-//        }
-
-//        var_dump( R::exec("UPDATE book SET title = :title WHERE id = 5", [
-//            ":title" => "new title #5"
-//        ]) );
-
-//        $books = R::getAll("SELECT * FROM book WHERE id > ?", [3]);
-//        debug($books);
-
         R::debug(1);
 
-//        $book = R::getRow("SELECT * FROM book LIMIT 1");
-//        $books = R::getCol("SELECt title FROM book");
-//        $book = R::getCell("SELECt title FROM book LIMIT 1");
-
-//        $books = R::getAll("SELECT id, title FROM book WHERE id > ?", [8]);
-//        $books = R::getAssoc("SELECT * FROM book WHERE id > ?", [8]);
 
 
-//        R::exec("INSERT INTO book(title, price, category_id) VALUES (?, ?, ?)", [
-//            "Wow, new boook",
-//            "7",
-//            "3",
-//        ]);
-//        $id = R::getInsertID();
-//        debug($id);
+//        $data = R::getLook()->look(
+//            "SELECT id, title FROM book WHERE id > ?",
+//            [5],
+//            ['id', 'title'],
+//            "\t<li value='%s'>%s</li>", 'strtoupper', "\n"
+//        );
+//
+//        echo "<ul>\n{$data}\n</ul>";
+
+//        $book = null;
+//        $didChangePrice = R::matchUp(
+//            "book",
+//            " title = ?",
+//            ["Test book Title 1020"],
+//            [
+//                "price" => "0"
+//            ],
+//            [
+//                "title" => "Test book Title 1020",
+//                "price" => 0,
+//                "category_id" => 1
+//            ],
+//            $book
+//        );
+//        debug($book);
+
+//        R::csv(
+//            "SELECT id, title FROM book WHERE id > ?",
+//            [8],
+//            ["ID", "TITLE"],
+//            __DIR__ . "/file.csv"
+//        );
+
 
     }
 
